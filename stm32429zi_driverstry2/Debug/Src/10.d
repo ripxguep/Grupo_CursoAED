bc8b14162c771338689adfa7dfa44a6adfd52f7d
Héctor César García-Carreño Martínez <80358751+hectorcesargarciacarreno@users.noreply.github.com>
2023-03-26 23:46:04 +0200

    Delete stm32429zi_driverstry2 directory

--- stm32429zi_driverstry2/Debug/Src/10.d
+++
@@ -1,9 +0,0 @@
-Src/10.o: ../Src/10.c \
- C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32f429zi.h \
- C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32429zi_gpio.h \
- C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32f429zi.h \
- C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32429zi_I2C.h
-C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32f429zi.h:
-C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32429zi_gpio.h:
-C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32f429zi.h:
-C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32429zi_I2C.h:
--- stm32429zi_driverstry2/Debug/drivers/src/stm32429zi_I2C.d
+++
@@ -1,9 +0,0 @@
-drivers/src/stm32429zi_I2C.o: ../drivers/src/stm32429zi_I2C.c \
- C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32f429zi.h \
- C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32429zi_gpio.h \
- C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32f429zi.h \
- C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32429zi_I2C.h
-C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32f429zi.h:
-C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32429zi_gpio.h:
-C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32f429zi.h:
-C:/Users/hcg-c/Downloads/EMB/My_workspace/target/stm32429zi_driverstry2/drivers/inc/stm32429zi_I2C.h:
